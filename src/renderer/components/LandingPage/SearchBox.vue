c7e32857162e9427513abe698e07809592d2fac5
Giovanni Naufal <g10v45+github@gmail.com>
2017-09-27 21:53:45 -0300

    Adds new component

---
+++ src/renderer/components/LandingPage/SearchBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="jumbotron">
+    <p>Use the search box and technology selector to find a tutorial you're looking for.</p>
+  <!-- Search Box Here -->
+  <!-- Radio Button Filters Here -->
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        name: 'landing-page',
+      }
+    }
+  }
+</script>
