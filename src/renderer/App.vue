41288a58abee41adccd0311c8fa9a6254df57793
Renan Andrade <eu@renanandrade.com.br>
2017-12-26 17:46:58 -0200

    first commit & add title

---
+++ src/renderer/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <app-title class="col"></app-title>
+    <app-search class="col"></app-search>
+  </div>
+</template>
+
+<script>
+  import Search from './components/Search.vue'
+  import Title from './components/Title.vue'
+
+  export default {
+    components: {
+      appTitle: Title,
+      appSearch: Search
+    }
+  }
+</script>
+
+<style lang="sass">
+  
+</style>
---
+++ src/renderer/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h3 class="text-center">
+      <i class="fas fa-microphone"></i>
+      Karaokleiton
+    </h3>
+  </section>
+</template>
+
+<style lang="sass" scoped>
+  .fa-microphone
+    font-size: 0.8em;
+    
+  h3
+    margin-top: 0.5rem;
+  
+</style>
